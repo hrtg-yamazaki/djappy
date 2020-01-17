f65c9a144d2ee77928ac6148343bf1d3df7ae3d5
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-01-17 15:40:57 +0900

    詳細ページの作成(バックエンド)
    ・記事詳細ページを作成しました。
    ・TOPページの最新記事項目から詳細ページへの遷移を可能にしました。

--- djappy/models.py
+++ djappy/models.py
@@ -1,8 +1,28 @@
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Article(models.Model):
     title = models.CharField(max_length=255)
     content = models.CharField(max_length=4095)
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+        null=True,
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        null=True,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
--- djappy/views.py
+++ djappy/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views import generic
 from .models import Article
 
@@ -6,3 +7,9 @@ from .models import Article
 class IndexView(generic.ListView):
     model = Article
     paginate_by = 6
+
+    def get_queryset(self):
+        return super().get_queryset().order_by('-created_at')
+
+class DetailView(generic.DetailView):
+    model = Article
