595f49d7f81a6fcd471b283964168fe905a25199
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-01-21 16:35:08 +0900

    デプロイに関する修正
    ・herokuへのデプロイに関するmiddlewareの記述を修正しました。

--- config/settings.py
+++ config/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoizeMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
