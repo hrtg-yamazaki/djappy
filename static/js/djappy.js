dd77a028ea66e519fb61fd7f01a99bc9057f77e2
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-01-22 21:20:24 +0900

    レスポンシブデザインに関するアップデート
    ・device-widthが変動した際に発生していたメニューボタンに関する不具合を修正しました。
    ・サイドバーと記事のリスト表示のパートを部分テンプレート化しました。

--- static/js/djappy.js
+++ static/js/djappy.js
@@ -1,6 +1,6 @@
 $(function(){
 
-
+  // **** PC表示のメニュー展開 **** //
   $(".header-menu").click(function(){
 
     $(this).next(".header-links").slideToggle();
@@ -18,13 +18,34 @@ $(function(){
   })
 
 
+  // **** タブレット・スマートフォンのメニュー展開 **** //
+  
   $(".header-sp-menu").click(function(){
+
     $(".hidden-menu").slideToggle();
+
   })
 
   $(".hidden-close-sp__button").click(function(){
+
     $(".hidden-menu").slideToggle();
+
   })
+
+
+  // **** device-width 変動時の表示調整 **** //
+
+  $(window).resize(function(){
+
+    var windowWidth = $(window).width();
+    if (windowWidth <= 700){
+      $('.header-menu').css('display', 'none');
+    } else {
+      $('.header-menu').css('display', 'inline');
+    }
+
+  })
+   
 
 })
 
