61aff15db12502385823c596dae75cf9d5f3bded
hrtg-yamazaki <hrtg.yamazaki@gmail.com>
2020-02-26 23:24:23 +0900

    ユニットテスト実行の準備
    ・djappy/tests.pyに、Articleのテストモデル生成のためのメソッドを定義しました。
    ・READMEの記述について一部修正を行いました。
    (commitには含みませんが、ここでテスト用DBを生成し、ローカル専用の設定ファイルにDB読込のための記述を追加しています。)

--- djappy/tests.py
+++ djappy/tests.py
@@ -1,3 +1,24 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Article
+
+
+def create_article(test_title, test_content):
+
+    now = timezone.now()
+    article = Article(
+        title=test_title,
+        content=test_content,
+        image="article_images/dawn.jpg",
+        author_id=1,
+        category_id=1,
+        created_at=now,
+        updated_at=now,
+    )
+
+
+class ArticleModelTests(TestCase):
+
+    pass
